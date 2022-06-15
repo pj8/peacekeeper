98e405299977350b9934aa91ff4cbce073fc7f66
Yuki Adachi <yuki777@gmail.comm>
2022-06-16 00:21:06 +0900

    Delete unnecessary code
    - Disable to inject https://cdn.jsdelivr.net/npm/sweetalert2

--- content.js
+++ content.js
@@ -28,12 +28,6 @@
 
         // toast
         if(config.toast){
-          let script = document.createElement('script');
-          script.type = 'text/javascript';
-          script.async = true;
-          script.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11.4.17/dist/sweetalert2.all.min.js';
-          let child = document.getElementsByTagName('script')[0];
-          child.parentNode.insertBefore(script, child);
           Swal.fire({
             position: config.toast.position,
             icon: config.toast.icon,
