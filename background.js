516a771dcd07063e497824e60d40a8c408f6df99
yuki777 <yuki777@gmail.com>
2024-01-23 17:11:34 +0700

    Fix cs

--- background.js
+++ background.js
@@ -1,33 +1,37 @@
-chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
   if (changeInfo.status !== "complete") return;
-  if(! tab.url) return;
+  if (!tab.url) return;
 
-  let configs = await getObjectFromStorage('configs');
-  if(! configs) return;
+  let configs = await getObjectFromStorage("configs");
+  if (!configs) return;
 
   let matchedConfig = getMatchedConfig(tab.url, configs);
-  chrome.tabs.sendMessage(tabId, { type: "notify", config: matchedConfig}, null);
+  chrome.tabs.sendMessage(
+    tabId,
+    { type: "notify", config: matchedConfig },
+    null
+  );
 });
 
-function getMatchedConfig(url, configs){
+function getMatchedConfig(url, configs) {
   let origin = new URL(url).origin;
-  return configs.find(config => origin.match(new RegExp(config.url)));
+  return configs.find((config) => origin.match(new RegExp(config.url)));
 }
 
-chrome.action.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url: 'options.html'});
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.tabs.create({ url: "options.html" });
 });
 
-chrome.runtime.onInstalled.addListener(function(details){
-  if(details.reason == "install"){
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
     chrome.runtime.openOptionsPage();
   }
 });
 
-const getObjectFromStorage = async function(key) {
+const getObjectFromStorage = async function (key) {
   return new Promise((resolve, reject) => {
     try {
-      chrome.storage.sync.get(key, function(value) {
+      chrome.storage.sync.get(key, function (value) {
         resolve(value[key]);
       });
     } catch (ex) {
